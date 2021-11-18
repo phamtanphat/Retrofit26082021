908ea0afbe0b814de7f43272a07478b545147996
phamtanphat <phatdroid94@gmail.com>
2021-11-18 21:29:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofit26082021/RetrofitClient.java
@@ -0,0 +1,46 @@
+package com.example.retrofit26082021;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient instance = null;
+    private Retrofit retrofit;
+
+    private RetrofitClient(){
+        retrofit = createRetrofit();
+    }
+
+    public static RetrofitClient getInstance(){
+        if (instance == null){
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+    private Retrofit createRetrofit(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .readTimeout(30 , TimeUnit.SECONDS)
+                            .writeTimeout(30, TimeUnit.SECONDS)
+                            .connectTimeout(30 , TimeUnit.SECONDS)
+                            .retryOnConnectionFailure(true)
+                            .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                            .build();
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://khoapham.vn/")
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        return retrofit;
+    }
+}
