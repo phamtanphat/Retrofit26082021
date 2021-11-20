b01095ee8717b1aea3c8ea7293313f75214434ab
phamtanphat <phatdroid94@gmail.com>
2021-11-20 20:30:16 +0700

    menu

--- app/src/main/java/com/example/retrofit26082021/MainActivity.java
+++ app/src/main/java/com/example/retrofit26082021/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.retrofit26082021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Demo1
-
+//        ApiRequest apiRequest = RetrofitClient.getInstance().createRequest();
+//
+//        Call<Demo1> callbackDemo1 = apiRequest.fetchDemo1();
+//        callbackDemo1.enqueue(new Callback<Demo1>() {
+//            @Override
+//            public void onResponse(Call<Demo1> call, Response<Demo1> response) {
+//                if (response.errorBody() != null){
+//                    try {
+//                        Log.d("BBB",response.errorBody().string());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }else{
+//                    Demo1 demo1 = response.body();
+//                    Log.d("BBB",demo1.toString());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Demo1> call, Throwable t) {
+//
+//            }
+//        });
 
     }
 }
